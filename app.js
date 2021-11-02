73c313eb8a6daadf3c8c3681199d38cd8ed41f4d
Vitali Filipchuk <vitali.filipchuk@gmail.com>
2021-11-02 23:47:49 +0200

    basic layout and styling

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const app = Vue.createApp({
-    template: '<h1>Random User Generator</h1>'
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'JohnDoe@mail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+        }
+    }
 });
 
 app.mount('#app');
